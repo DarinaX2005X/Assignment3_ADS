61eb05ede8ae64873aac32c4d43c85d406343eb3
Darina <darinax2005x@gmail.com>
2024-05-07 18:31:55 +0500

    Added printBuckets

--- MyHashTable.java
+++ MyHashTable.java
@@ -22,15 +22,16 @@ public class MyHashTable<K, V> {
     private int capacity = 11;
     private int M = (int)(capacity * loadFactor);
     public MyHashTable() {
-        this(11, 0.75);
+        this(11, 0.85);
     }
 
     public MyHashTable(int capacity) {
-        this(capacity, 0.75);
+        this(capacity, 0.85);
     }
 
     public MyHashTable(int capacity, double loadFactor) {
         this.capacity = capacity;
+        this.loadFactor = loadFactor;
         this.chainArray = new HashNode[capacity];
         this.M = (int)(capacity*loadFactor);
     }
@@ -46,20 +47,45 @@ public class MyHashTable<K, V> {
             chainArray[index] = newNode;
         } else {
             HashNode<K, V> current = chainArray[index];
-            do {
+            while (current.next != null) {
                 if (current.key.equals(key)) {
                     current.value = value;
                     return;
                 }
-                if(current.next == null) {
-                    current.next = newNode;
-                }
                 current = current.next;
-            } while(current != null);
+            };
+            if (current.key.equals(key)) {
+                if (current.value.equals(value))
+                    System.out.println(value);
+                current.value = value;
+                return;
+            }
+            current.next = newNode;
         }
         size++;
-    }
 
+        //if (size >= M)
+         //   rehash();
+    }
+    private void rehash() {
+        int newCapacity = capacity*2 + 1;
+        HashNode<K,V>[] newChainArray = new HashNode[newCapacity];
+
+        for (int i = 0; i < chainArray.length ; i++) {
+            HashNode<K,V> entry = chainArray[i];
+            while (entry != null) {
+                HashNode<K, V> newEntry = entry;
+                entry = entry.next;
+                int newIndex = hash(newEntry.key);
+                newEntry.next = newChainArray[newIndex];
+                newChainArray[newIndex] = newEntry;
+            }
+        }
+
+        chainArray = newChainArray;
+        capacity = newCapacity;
+        M = (int) (newCapacity * loadFactor);
+    }
     public V get(K key) {
         int index = hash(key);
         HashNode<K, V> current = chainArray[index];
@@ -107,5 +133,19 @@ public class MyHashTable<K, V> {
             }
         }
         return null;
+    }
+
+    public void printBuckets() {
+        System.out.println("Capacity: "+ capacity);
+        for (int i = 0; i < chainArray.length ; i++) {
+            int bucketSize = 0;
+            HashNode<K,V> entry = chainArray[i];
+            while(entry != null) {
+                bucketSize++;
+                entry = entry.next;
+            }
+
+            System.out.println(i + ": bucketSize = " + bucketSize);
+        }
     }
 }
